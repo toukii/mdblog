712209516d18613f4124d2ee719f3fec0cfb4b66
时永宾 <ybshi1@chinaums.com>
2017-05-02 15:36:03 +0800

    auto

--- Linux/bash/t1.sh
+++ Linux/bash/t1.sh
@@ -3,6 +3,8 @@
 echo $0
 # 第一个参数
 echo $1
+# 所有参数
+echo $@
 # 参数格式
 echo $#
 # 上个命令的返回值
@@ -38,11 +40,51 @@ echo "nil user"
 ;;
 esac
 
-# loop
-
+## loop
+# while
 count=1
-while test $count -le 100
+while test $count -le 10
 do
 	echo $count
 	count=$[$count+1]
 done
+
+## dead loop
+# while :
+# do
+# 	echo -n "."
+# 	sleep 1
+# done
+
+# for
+for((i=0;i<10;i++))
+do
+	echo $i
+done
+
+for i in `seq 1 2 10`
+do
+	echo $i
+done
+
+for i in {1..10}
+do
+	echo $i
+done
+
+# array
+# 声明
+declare -a arr
+# 赋值
+arr[0]=1
+arr[2]=a
+# 第一个元素
+echo ${arr[0]}
+# 所有元素
+echo ${arr[*]}
+# 长度
+echo ${#arr[*]}
+
+#执行顺序：别名－特殊内建命令－函数－内建命令－外部命令
+
+[参考](http://www.cnblogs.com/end/archive/2012/09/25/2701915.html)
